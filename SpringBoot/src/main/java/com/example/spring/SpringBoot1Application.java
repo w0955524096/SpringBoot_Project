c158bec46d957235b9c24cb4de427ca54664ce83
w0955524096 <ptree30202@gmail.com>
2020-08-07 13:08:25 +0800

    change err

--- SpringBoot/src/main/java/com/example/spring/SpringBoot1Application.java
+++ SpringBoot/src/main/java/com/example/spring/SpringBoot1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApplication1 {
+public class SpringBoot1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApplication.class, args);
+        SpringApplication.run(SpringBoot1Application.class, args);
     }
 
 }
